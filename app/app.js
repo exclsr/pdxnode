8c8766edaaf385c5da316cc93fd0cb556d0f7680
Phil Manijak <github@exclsr.com>
2013-12-26 17:19:02 -0800

    Deprecated: Redirect to pdxnode.org
    The site on pdxnode.jit.su is gone, and redirects,
    permanently to pdxnode.org.

--- app/app.js
+++ app/app.js
@@ -4,7 +4,6 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
   , http = require('http')
   , path = require('path');
 
@@ -12,14 +11,11 @@ var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
@@ -27,7 +23,10 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('*', function (req, res) {
+  res.setHeader('Location', 'http://pdxnode.org');
+  res.send(301)
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
